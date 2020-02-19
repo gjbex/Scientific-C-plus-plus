5ef573378e67269122378e52afb5337c7561a1f8
Geert Jan Bex <geertjan.bex@uhasselt.be>
2020-02-19 18:26:15 +0100

    Remove unwanted files

--- source-code/STL/Knapsack/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "knapsack.h"
-
-int main() {
-    auto stuff = fill_stuff_random(15);
-    for (const auto& item: stuff)
-        std::cout << item << "\n";
-    return 0;
-}

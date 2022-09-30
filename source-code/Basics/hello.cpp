9b6d1ee11129691aad5ab4e0da4e532d699e2a47
Jaakko Ahola <jaakko.ahola@virnex.fi>
2022-09-30 15:40:58 +0300

    Removed .cpp changes

--- source-code/Basics/hello.cpp
+++ source-code/Basics/hello.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int main(int argc, char *argv[]) {
     if (argc > 1)
-        cout << "hello " << argv[1] << " " << argv[2] << "!" << endl;
+        std::cout << "hello " << argv[1] << "!" << std::endl;
     else
-        cout << "hello world!" << endl;
-
-    int m {3}, n {5};
-    m += n++; // post
-    cout << m << " " << n << endl;
-    int k {3}, l {5};
-    k += ++l;  // pre
-    cout << k << " " << l << endl;
+        std::cout << "hello world!" << std::endl;
     return 0;
-    /*
-    multilinecomment
-    */
 }

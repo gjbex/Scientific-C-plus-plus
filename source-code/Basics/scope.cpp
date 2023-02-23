43c66a884d17a21b576570bf2903e429c5ac781f
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-02-23 09:25:18 +0100

    Extend scope example

--- source-code/Basics/scope.cpp
+++ source-code/Basics/scope.cpp
@@ -6,11 +6,13 @@ int main() {
     int i {3};
     cout << "out of block: " << i << endl;
     {
+        cout << "in block before declaration: " << i << endl;
         int i {5};
-        cout << "in block: " << i <<  endl;
+        cout << "in block after declaration: " << i <<  endl;
     }
+    cout << "out of block before for: " << i << endl;
     for (int i = 10; i < 13; i++)
         cout << "in for: " << i << endl;
-    cout << "out of block: " << i << endl;
+    cout << "out of block after for: " << i << endl;
     return 0;
 }

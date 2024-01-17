94e034999d704f1a96413b555984f7db164c0405
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-01-17 17:32:11 +0100

    Transform to template

--- source-code/UserDefinedTypes/Matrices/README.md
+++ source-code/UserDefinedTypes/Matrices/README.md
@@ -7,7 +7,6 @@ to illustrate the rule of five.
 ## What is it?
 
 1. `matrices.h`: class declaration.
-1. `matrices.cpp`: method definitions.
 1. `matrix_test.cpp`: main function to illustrate the usage
    of the class.
 1. `CMakeLists.txt`: CMake file to build the applications.
--- source-code/UserDefinedTypes/Matrices/matrix_test.cpp
+++ source-code/UserDefinedTypes/Matrices/matrix_test.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "matrices.h"
 
-Matrix create_matrix(int rows, int cols) {
-    Matrix m(rows, cols);
+Matrix<double> create_matrix(int rows, int cols) {
+    Matrix<double> m(rows, cols);
     for (int i = 0; i < m.rows(); ++i) {
         for (int j = 0; j < m.cols(); ++j) {
             m(i, j) = i * m.cols() + j;
@@ -12,13 +12,13 @@ Matrix create_matrix(int rows, int cols) {
 }
 
 int main() {
-    Matrix m = create_matrix(4, 3);
+    Matrix<double> m = create_matrix(4, 3);
     std::cout << "m = " << std::endl;
     std::cout << m << std::endl;
-    Matrix m2 = m;
+    Matrix<double> m2 = m;
     std::cout << "m2 = " << std::endl;
     std::cout << m2 << std::endl;
-    Matrix m3(4, 3);
+    Matrix<double> m3(4, 3);
     m3 = m;
     std::cout << "m3 = " << std::endl;
     std::cout << m3 << std::endl;

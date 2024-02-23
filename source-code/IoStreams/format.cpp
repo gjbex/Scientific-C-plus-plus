65b191f6b35415622e2610837cb32909a3a9e6dc
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-02-23 01:32:43 +0100

    Add floating point format example

--- source-code/IoStreams/format.cpp
+++ source-code/IoStreams/format.cpp
@@ -1,8 +1,12 @@
+#include <cmath>
 #include <format>
 #include <iostream>
 #include <string>
 
 int main() {
-  std::string s = std::format("The answer is {}.", 42);
-  std::cout << s << std::endl;
+  std::string s1 = std::format("The answer is {}.", 42);
+  std::cout << s1 << std::endl;
+  const double PI {std::acos(-1)};
+  std::string s2 = std::format("PI is {:.3f}.", PI);
+  std::cout << s2 << std::endl;
 }

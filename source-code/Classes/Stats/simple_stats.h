2502b4b2f9629d9af18d5a56dfecb4ec75d70dde
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-04-20 12:07:56 +0200

    Add exceptions for mean and stddev methods

--- source-code/Classes/Stats/simple_stats.h
+++ source-code/Classes/Stats/simple_stats.h
@@ -2,6 +2,7 @@
 #define SIMPLE_STATS_HDR
 
 #include <iostream>
+#include <stdexcept>
 
 class SimpleStats {
     private:
@@ -12,7 +13,12 @@ class SimpleStats {
         void add(double x) { sum_ += x; n_++; };
         double sum() const { return sum_; };
         int n() const { return n_; };
-        double mean() const { return sum()/n(); };
+        double mean() const {
+            if (n_ == 0) {
+                throw std::domain_error("SimpleStats::mean(): no data");
+            }
+            return sum_ / n_;
+        };
         friend std::ostream& operator<<(std::ostream& out,
                                         const SimpleStats& stats);
 };
--- source-code/Classes/Stats/stats.cpp
+++ source-code/Classes/Stats/stats.cpp
@@ -1,9 +1,13 @@
 #include <cmath>
+#include <stdexcept>
 
 #include "stats.h"
 
 double Stats::stddev() const {
     using std::sqrt;
+    if (n() < 2) {
+        throw std::domain_error("Stats::stddev(): at least two data points required");
+    }
     return sqrt((sum2() - sum()*sum()/n())/(n() - 1));
 }
 

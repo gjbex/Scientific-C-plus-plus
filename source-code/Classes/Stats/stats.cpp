898e74b8314a8bc1a92edc0d5bbf5f3681f3ec62
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-04-20 12:18:25 +0200

    Cast to base-class reference to avoid slicing in operator<<

--- source-code/Classes/Stats/stats.cpp
+++ source-code/Classes/Stats/stats.cpp
@@ -12,6 +12,6 @@ double Stats::stddev() const {
 }
 
 std::ostream& operator<<(std::ostream& out, const Stats& stats) {
-    return out << static_cast<SimpleStats>(stats) << std::endl
+    return out << static_cast<const SimpleStats&>(stats) << std::endl
                << "sum2 = " << stats.sum2();
 }

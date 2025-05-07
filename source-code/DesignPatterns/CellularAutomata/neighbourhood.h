9dcb56e7d5bb242bc07f190dda4358ba7b9900b5
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-05-07 18:03:23 +0200

    Merge neighbourhood index computation into rule definition

--- source-code/DesignPatterns/CellularAutomata/neighbourhood.h
+++
@@ -1,8 +0,0 @@
-#ifndef NEIGHBOURHOOD_HDR
-#define NEIGHBOURHOOD_HDR
-
-#include "cells.h"
-
-int neighbourhood_index(const Cells& cells, std::size_t index);
-
-#endif
--- source-code/DesignPatterns/CellularAutomata/rule.h
+++ source-code/DesignPatterns/CellularAutomata/rule.h
@@ -4,8 +4,11 @@
 #include <array>
 #include <cstdint>
 
+#include "cells.h"
+
 using Rule = std::array<int8_t, 8>;
 
 Rule create_rule(uint8_t rule_nr);
+int neighbourhood_index(const Cells& cells, std::size_t index);
 
 #endif

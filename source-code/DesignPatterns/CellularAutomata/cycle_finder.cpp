bc6ca67e7400d084ba4754fde34f3a30fc5bc792
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-05-08 16:43:43 +0200

    Add reset at start of run

--- source-code/DesignPatterns/CellularAutomata/cycle_finder.cpp
+++ source-code/DesignPatterns/CellularAutomata/cycle_finder.cpp
@@ -1,6 +1,8 @@
 #include "cycle_finder.h"
 
 void CycleFinder::run(Dynamics& dynamics, Cells& cells) {
+    step_ = 0;
+    states_.clear();
     while (!has_cycle(cells)) {
         states_[cells_to_key(cells)] = step_;
         dynamics.update(cells);

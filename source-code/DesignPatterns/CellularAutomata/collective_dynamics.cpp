b597806dc8a90b2b3d3f317a22d83c2ad191544a
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-05-09 15:20:13 +0200

    Rename CyclicBoundaryDynamics to CollectiveDynamics

--- source-code/DesignPatterns/CellularAutomata/collective_dynamics.cpp
+++ source-code/DesignPatterns/CellularAutomata/collective_dynamics.cpp
@@ -1,6 +1,6 @@
-#include "cyclic_boundary_dynamics.h"
+#include "collective_dynamics.h"
 
-void CyclicBoundaryDynamics::update(Cells& cells) {
+void CollectiveDynamics::update(Cells& cells) {
     for (std::size_t i = 0; i < cells.size(); ++i) {
         std::size_t index = neighbourhood_index(cells, i);
         new_cells_[i] = rule_[index];

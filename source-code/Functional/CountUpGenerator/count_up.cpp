0da9431e6bff69127a74657e4890380cb36930da
Geert Jan Bex <geertjan.bex@uhasselt.be>
2026-04-09 20:50:16 +0200

    Fix typos

--- source-code/Functional/CountUpGenerator/count_up.cpp
+++ source-code/Functional/CountUpGenerator/count_up.cpp
@@ -5,7 +5,7 @@
 #include <utility>
 
 
-auto create_count_up_geneartor(const uint64_t max_value) {
+auto create_count_up_generator(const uint64_t max_value) {
     return [max_value] () -> std::generator<uint64_t> {
         uint64_t current_value {0};
         while (current_value < max_value) {
@@ -16,8 +16,8 @@ auto create_count_up_geneartor(const uint64_t max_value) {
 }
 
 int main() {
-    auto geneartor {create_count_up_geneartor(10)};
-    for (auto i : geneartor()) {
+    auto generator {create_count_up_generator(10)};
+    for (auto i : generator()) {
         std::cout << i << ' ';
     }
     std::cout << '\n';
